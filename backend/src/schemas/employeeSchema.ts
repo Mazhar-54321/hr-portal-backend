ced627994826764cba41387d000352e524f5b4fc
Mazhar-54321 <syedmazharali742@gmail.com>
2025-11-10 18:37:39 +0530

    Added zod employee schema

---
+++ backend/src/schemas/employeeSchema.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+export const employeeSchema = z.object({
+  name: z.string().min(3).max(50),
+  username: z.string().min(3).max(20),
+  email: z.email().max(100),
+  phone: z.string().max(20).optional(),
+  website: z.url().max(100).optional(),
+  role: z.enum(["Admin", "Editor", "Viewer"]),
+  isActive: z.boolean(),
+  skills: z.array(z.string().min(2).max(10)).nonempty(),
+  availableSlots: z.array(z.string()),
+  address: z.object({
+    street: z.string().min(5).max(100),
+    city: z.string().min(2).max(50),
+    zipcode: z.string().regex(/^[0-9]{5,10}$/),
+  }),
+  company: z.object({
+    name: z.string().min(2).max(100),
+  }),
+});
